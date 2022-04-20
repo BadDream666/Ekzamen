4338a84fd9a76f55dd79d2e2581b903a6ca5ad7f
orugam1 <86592742+gfdhshfs@users.noreply.github.com>
2022-04-20 09:47:41 +0300

    ekzamennn

---
+++ ClassLibrary1/Application.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class Application
+    {
+        private Application application;
+        private List<TaskList> taskList;
+    }
+
+    public Application()
+    {
+        TaskList taskList = new TaskList("Входящие");
+        TaskLists = new List<TaskList>();
+        taskList.Add(taskList);
+    }
+
+    public Application GetApplication()
+    {
+        if (application != null)
+            return application
+
+        application = new Application();
+        return Application;
+    }
+
+    public void CreateTaskList(string name)
+    {
+
+    }
+}
---
+++ ClassLibrary1/Task.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        public DateTime Due { get; set;}
+    }
+}
